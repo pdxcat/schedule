9fc8af4c91905b4e6b117e9d63d071caf6352a65
Jason Owen <jason.a.owen@gmail.com>
2012-08-14 18:33:56 -0700

    clean up all the whitespace

--- ns_drop_shifts.php
+++ ns_drop_shifts.php
@@ -1,75 +1,66 @@
 <?php
-require('ns_common.php');
-check_session();
+  require('ns_common.php');
+  check_session();
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
 <html>
 
 <head>
-
-<?php
-$username = $_SESSION['username'];
-echo "<title>The Schedule ($username)</title>";
-?>
-
-<link rel="stylesheet" type="text/css" href="ns_general.css" media="screen" />
-
+  <?php
+    $username = $_SESSION['username'];
+    echo "<title>The Schedule ($username)</title>";
+  ?>
+  <link rel="stylesheet" type="text/css" href="ns_general.css" media="screen" />
 </head>
 
 <body>
-
 <div id="page-container">
-
 <div id="top-nav">
 <?php
-include 'ns_top_navigation.php';
+  include 'ns_top_navigation.php';
 ?>
 
 <div id="content">
 <?php
 if ($username) {
-	// do stuff for the currently logged in user
-	printf("Logged in as %s. <br />", $username);
-	
+  // do stuff for the currently logged in user
+  printf("Logged in as %s. <br />", $username);
 
-	if (empty($_POST['operation'])) {
-		// The user really shouldn't get to this page without having
-		// clicked a button on the view/drop shifts page.
+  if (empty($_POST['operation'])) {
+    // The user really shouldn't get to this page without having
+    // clicked a button on the view/drop shifts page.
 
-		echo "No operation to perform. <br />";
-		echo "<a href=\"ns_show_schedule.php\">Return to your schedule</a><br />";
-		
-	} elseif (is_array($_SESSION['drop_shifts']) && $_POST['operation'] == "Proceed") {
-		// If the user has confirmed dropping the shifts then proceed
-		// and display a confirmation and link back to the calendar
-		// view.
+    echo "No operation to perform. <br />";
+    echo "<a href=\"ns_show_schedule.php\">Return to your schedule</a><br />";
+  } elseif (is_array($_SESSION['drop_shifts']) && $_POST['operation'] == "Proceed") {
+    // If the user has confirmed dropping the shifts then proceed
+    // and display a confirmation and link back to the calendar
+    // view.
 
-		$dbh = start_db();
+    $dbh = start_db();
 
-		foreach ($_SESSION['drop_shifts'] as $key => $val) {
-			$drop_shifts_ids[] = $key;
-		};
+    foreach ($_SESSION['drop_shifts'] as $key => $val) {
+      $drop_shifts_ids[] = $key;
+    };
 
-		drop_shifts_by_sa_ids($drop_shifts_ids, $dbh);
-	
-		$dbh = null;
-	
-		echo "Shifts dropped. <br />";
-		echo "<a href=\"ns_show_schedule.php\">Back to your schedule</a><br />";
-		session_unset($_SESSION['drop_shifts']);
+    drop_shifts_by_sa_ids($drop_shifts_ids, $dbh);
 
-	} else {
-		// Shouldn't ever really get here.
-	
-		echo "Something went wrong. Contact the scheduler. <br />";
-	};
-	
+    $dbh = null;
+
+    echo "Shifts dropped. <br />";
+    echo "<a href=\"ns_show_schedule.php\">Back to your schedule</a><br />";
+    session_unset($_SESSION['drop_shifts']);
+
+  } else {
+    // Shouldn't ever really get here.
+    echo "Something went wrong. Contact the scheduler. <br />";
+  };
 } else {
-	// login failure
-	echo "Fail.<br />";
+  // login failure
+  echo "Fail.<br />";
 };
 ?>
 
@@ -80,85 +71,83 @@ if ($username) {
 
 <?php
 function discard_dropped_sa_ids($sa_ids, &$dbh) {
-	/*
-	Fetch any ns_shift_dropped entries which match the provided sa_ids and
-	remove the matching sa_ids from the provided array.
-	*/
+  /*
+    Fetch any ns_shift_dropped entries which match the provided sa_ids and
+    remove the matching sa_ids from the provided array.
+  */
+  $sa_id_list .= implode(",",$sa_ids);
 
-	$sa_id_list .= implode(",",$sa_ids);
+  /*
+    Set up and run query for grabbing the sa_ids from the provided list
+    which have an associated shift drop entry.
+  */
+  $token_str = make_token_string($sa_ids);
 
-	/*
-	Set up and run query for grabbing the sa_ids from the provided list
-	which have an associated shift drop entry.
-	*/ 
+  $query = "
+    SELECT d.ns_sa_id
+    FROM ns_shift_dropped as d
+    WHERE d.ns_sa_id IN ($token_str)";
 
-	$token_str = make_token_string($sa_ids);
+  $sth = $dbh->prepare($query);
 
-	$query = "
-		SELECT d.ns_sa_id
-		FROM ns_shift_dropped as d
-		WHERE d.ns_sa_id IN ($token_str)";
-	
-	$sth = $dbh->prepare($query);
+  // Binds the items in the array to the tokens in the query.
+  bind_id_list($sa_ids, $sth);
 
-	// Binds the items in the array to the tokens in the query.
-	bind_id_list($sa_ids, $sth);
+  $sth->execute();
 
-	$sth->execute();
+  $sth->setFetchMode(PDO::FETCH_ASSOC);
 
-	$sth->setFetchMode(PDO::FETCH_ASSOC);
+  // If we got no matches break here.
+  if ($sth->rowCount() == 0) {
+    return $sa_ids;
+  };
 
-	// If we got no matches break here.
-	if ($sth->rowCount() == 0) {
-		return $sa_ids;
-	}; 
+  // Iterate through the results and remove from the sa_ids list any ids
+  // matching the ones retrieved from the database.
+  while ($row = $sth->fetch()) {
+    foreach ($sa_ids as $key => $val) {
+      if ($val == $db_row['ns_sa_id']) {
+        unset($sa_ids[$key]);
+      };
+    };
+  };
 
-	// Iterate through the results and remove from the sa_ids list any ids
-	// matching the ones retrieved from the database.
-	while ($row = $sth->fetch()) {
-		foreach ($sa_ids as $key => $val) {
-			if ($val == $db_row['ns_sa_id']) {
-				unset($sa_ids[$key]);
-			};
-		};
-	};
+  // Reset array indexes since they'll be messed up if we removed any
+  // elements from the middle.
+  $sa_ids = array_values($sa_ids);
 
-	// Reset array indexes since they'll be messed up if we removed any
-	// elements from the middle.
-	$sa_ids = array_values($sa_ids);
-
-	return $sa_ids;
+  return $sa_ids;
 };
 
 
 function drop_shifts_by_sa_ids (&$drop_shifts, &$dbh) {
-	/*
-	For each id in the array passed to the function, do the following:
-	-Remove sa_ids which already have an ns_shift_dropped entry from the
-	 list of sa_ids to drop. 
-	-Add a new entry to ns_shift_dropped for the shift assignment.
-	-Use the current date/time as the ns_sd_droptime.
-	*/ 	
+  /*
+    For each id in the array passed to the function, do the following:
+     - Remove sa_ids which already have an ns_shift_dropped entry from the
+       list of sa_ids to drop.
+     - Add a new entry to ns_shift_dropped for the shift assignment.
+     - Use the current date/time as the ns_sd_droptime.
+  */
 
-	// Run function to discard from the array shift assignment ids which
-	// have already been dropped. 
-	$drop_shifts = discard_dropped_sa_ids($drop_shifts, $dbh);
-	
-	// Insert new ns_shift_dropped entries for each of the remaining sa_ids.
-	$query = "
-		INSERT INTO `ns_shift_dropped` (ns_sa_id,ns_sd_droptime)
-		VALUES (:sa_id, :timestamp)";
+  // Run function to discard from the array shift assignment ids which
+  // have already been dropped.
+  $drop_shifts = discard_dropped_sa_ids($drop_shifts, $dbh);
 
-	$sth = $dbh->prepare($query);
+  // Insert new ns_shift_dropped entries for each of the remaining sa_ids.
+  $query = "
+    INSERT INTO `ns_shift_dropped` (ns_sa_id,ns_sd_droptime)
+    VALUES (:sa_id, :timestamp)";
 
-	foreach ($drop_shifts as $shift) {
-		$now = date_create();
-		$timestamp = date_format($now,"Y-m-d H:i:s");
-		
-		$sth->bindParam(':sa_id',$shift);
-		$sth->bindParam(':timestamp',$timestamp);
+  $sth = $dbh->prepare($query);
 
-		$sth->execute();
-	};
+  foreach ($drop_shifts as $shift) {
+    $now = date_create();
+    $timestamp = date_format($now,"Y-m-d H:i:s");
+
+    $sth->bindParam(':sa_id',$shift);
+    $sth->bindParam(':timestamp',$timestamp);
+
+    $sth->execute();
+  };
 };
 ?>
--- public/ns_public_weekly_master.php
+++ public/ns_public_weekly_master.php
@@ -4,9 +4,7 @@
 <html>
 
 <head>
-
-<link rel="stylesheet" type="text/css" href="ns_cal_public.css" media="screen" />
-
+  <link rel="stylesheet" type="text/css" href="ns_cal_public.css" media="screen" />
 </head>
 
 <body>
@@ -21,14 +19,14 @@ $dbh = start_db();
 // Determine first and last days of the week.
 $first_of_week = clone($base_date);
 if (date_format($first_of_week, 'w') > 1) {
-	// If the base date is after Monday, roll back $first_of_week until we
-	// hit Monday.
-	while (date_format($first_of_week, 'w') != 1) {
-		date_modify($first_of_week, '-1 day');
-	};
+  // If the base date is after Monday, roll back $first_of_week until we
+  // hit Monday.
+  while (date_format($first_of_week, 'w') != 1) {
+    date_modify($first_of_week, '-1 day');
+  };
 } elseif (date_format($first_of_week, 'w') == 0) {
-	// If the base date is Sunday, add 1 day to it to get Monday.
-	date_modify($first_of_week, '+1 day');
+  // If the base date is Sunday, add 1 day to it to get Monday.
+  date_modify($first_of_week, '+1 day');
 };
 
 $last_of_week = clone($first_of_week);
@@ -59,36 +57,34 @@ echo "</span>";
 <?php
 // Loop over each hour in the work day
 for ($hour = 8; $hour <= 17; $hour++) {
-	// Loop over each day of the week being viewed
-	for ($current_date = clone($first_of_week);
-	date_diff(date_format($current_date,'Y-m-d'),date_format($last_of_week,'Y-m-d')) >= 0 ;
-	date_modify($current_date,'+1 day')) {
-		// If day is Monday, begin a new row and write out the hour
-		// label.
-		if (date_format($current_date,'w') == 1) {
-			echo "<tr>";
-			echo "<td>";
-			echo sprintf("%02d", $hour) . "00";
-			echo "</td>";
-		};
+  // Loop over each day of the week being viewed
+  for ($current_date = clone($first_of_week);
+  date_diff(date_format($current_date,'Y-m-d'),date_format($last_of_week,'Y-m-d')) >= 0 ;
+  date_modify($current_date,'+1 day')) {
+    // If day is Monday, begin a new row and write out the hour label.
+    if (date_format($current_date,'w') == 1) {
+      echo "<tr>";
+      echo "<td>";
+      echo sprintf("%02d", $hour) . "00";
+      echo "</td>";
+    };
 
-		$flags = array('dh' => 0, 'kn' => 0);
-		write_table_cell($current_date, $hour, $shifts, $flags);
+    $flags = array('dh' => 0, 'kn' => 0);
+    write_table_cell($current_date, $hour, $shifts, $flags);
 
-		// If day is Saturday, cap off the row after writing out the
-		// cell contents.
-		if (date_format($current_date,'w') == 6) {
-			echo "</tr>";
-		};
+    // If day is Saturday, cap off the row after writing out the cell contents.
+    if (date_format($current_date,'w') == 6) {
+      echo "</tr>";
+    };
 
-		// Hackish shit necessary due to the hackishness of the
-		// date_diff function I'm using as a stand in for the
-		// real thing since we're running ancient ass PHP.
-		if (date_diff(date_format($current_date,'Y-m-d'),
-		date_format($last_of_week,'Y-m-d')) == 0) {
-			break;
-		};
-	};
+    // Hackish shit necessary due to the hackishness of the
+    // date_diff function I'm using as a stand in for the
+    // real thing since we're running ancient ass PHP.
+    if (date_diff(date_format($current_date,'Y-m-d'),
+    date_format($last_of_week,'Y-m-d')) == 0) {
+      break;
+    };
+  };
 };
 ?>
 
@@ -96,9 +92,9 @@ for ($hour = 8; $hour <= 17; $hour++) {
 
 <br />
 <span class="cal_labels">
-Schedule key: <br />
-<span class="shift_dh">DOGHaus	FAB 82-01</span><br />
-<span class="shift_kn">Kennel	EB 325A</span><br />
+  Schedule key: <br />
+  <span class="shift_dh">DOGHaus  FAB 82-01</span><br />
+  <span class="shift_kn">Kennel EB 325A</span><br />
 </span>
 
 </div>
@@ -107,117 +103,117 @@ Schedule key: <br />
 
 <?php
 function start_db() {
-        // Establishes the database connection.
-        // Uses an include file with some special permissions on it.
-        require('db.inc');
-        $db_database = "schedule";
-        try {
-                $dbh = new PDO("mysql:host=$db_host;dbname=$db_database",$db_user,$db_password);
-        } catch (PDOException $e) {
-                echo $e->getMessage();
-        };
+  // Establishes the database connection.
+  // Uses an include file with some special permissions on it.
+  require('db.inc');
+  $db_database = "schedule";
+  try {
+    $dbh = new PDO("mysql:host=$db_host;dbname=$db_database",$db_user,$db_password);
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+  };
 
-        return $dbh;
+  return $dbh;
 };
 
 
 function write_table_cell(&$date, &$hour, &$gwt_shifts, &$flags) {
-	$db_date = date_format($date, 'Y-m-d');
-	$start_time = sprintf('%02d',$hour) . ":00:00";
-	echo "<td>";
-	foreach ($gwt_shifts as $date_key => $date_val) {
-		if ($date_key == $db_date) {
-			foreach ($date_val as $time_key => $time_val) {
-				if ($time_key == $start_time) {
-					foreach ($time_val as $assignment) {
-						if ($assignment['ns_desk_shortname'] == "Kennel"
-							&& $flags['kn'] == 0) {
-							echo "<span class=\"shift_kn\">";
-							echo " @ ";
-							echo "</span>";
-							$flags['kn'] = 1;
-						} elseif ($assignment['ns_desk_shortname'] == "DOGHaus"
-							&& $flags['dh'] == 0) {
-							echo "<span class=\"shift_dh\">";
-							echo " @ ";
-							echo "</span>";
-							$flags['dh'] = 1;
-						};
-					};
-				};
-			};
-		};
-	};
+  $db_date = date_format($date, 'Y-m-d');
+  $start_time = sprintf('%02d',$hour) . ":00:00";
+  echo "<td>";
+  foreach ($gwt_shifts as $date_key => $date_val) {
+    if ($date_key == $db_date) {
+      foreach ($date_val as $time_key => $time_val) {
+        if ($time_key == $start_time) {
+          foreach ($time_val as $assignment) {
+            if ($assignment['ns_desk_shortname'] == "Kennel"
+              && $flags['kn'] == 0) {
+              echo "<span class=\"shift_kn\">";
+              echo " @ ";
+              echo "</span>";
+              $flags['kn'] = 1;
+            } elseif ($assignment['ns_desk_shortname'] == "DOGHaus"
+              && $flags['dh'] == 0) {
+              echo "<span class=\"shift_dh\">";
+              echo " @ ";
+              echo "</span>";
+              $flags['dh'] = 1;
+            };
+          };
+        };
+      };
+    };
+  };
 
-	echo "</td>";
+  echo "</td>";
 };
 
 
 /*
-Using this since we're currently runing PHP 5.2.mumble. It's taken from an
-example posted at http://www.php.net/manual/en/datetime.diff.php by Dennis C.
+  Using this since we're currently runing PHP 5.2.mumble. It's taken from an
+  example posted at http://www.php.net/manual/en/datetime.diff.php by Dennis C.
 */
 
 function date_diff($date1, $date2) {
-    $current = $date1;
-    $datetime2 = date_create($date2);
-    $count = 0;
-    while(date_create($current) < $datetime2){
-        $current = gmdate("Y-m-d", strtotime("+1 day", strtotime($current)));
-        $count++;
-    }
-    return $count;
+  $current = $date1;
+  $datetime2 = date_create($date2);
+  $count = 0;
+  while(date_create($current) < $datetime2){
+    $current = gmdate("Y-m-d", strtotime("+1 day", strtotime($current)));
+    $count++;
+  }
+  return $count;
 }
 
 
 function get_shifts_for_all( &$start_date, &$end_date, &$dbh ) {
-        /*
-        Function to fetch all active assignments in a specified date range.
-        This is used to populate the weekly schedule displays which show when
-        people are on shift.
+  /*
+    Function to fetch all active assignments in a specified date range.
+    This is used to populate the weekly schedule displays which show when
+    people are on shift.
 
-        Format for return array looks like:
-        $return_array
-        ['ns_shift_date']
-                ['ns_shift_start_time']
-                        ['ns_cat_uname']
-                        ['ns_desk_shortname']
-        */
+    Format for return array looks like:
+    $return_array
+      ['ns_shift_date']
+      ['ns_shift_start_time']
+      ['ns_cat_uname']
+      ['ns_desk_shortname']
+  */
 
-        $gsfa_start_date = date_format($start_date, 'Y-m-d');
-        $gsfa_end_date = date_format($end_date, 'Y-m-d');
+  $gsfa_start_date = date_format($start_date, 'Y-m-d');
+  $gsfa_end_date = date_format($end_date, 'Y-m-d');
 
-        $query = "
-                SELECT s.ns_shift_date, s.ns_shift_start_time, c.ns_cat_uname, d.ns_desk_shortname
-                FROM ns_shift as s, ns_shift_assigned as a, ns_desk as d, ns_cat as c
-                WHERE s.ns_shift_date >= ?
-                AND s.ns_shift_date <= ?
-                AND a.ns_cat_id = c.ns_cat_id
-                AND a.ns_desk_id = d.ns_desk_id
-                AND a.ns_shift_id = s.ns_shift_id
-                AND a.ns_sa_id NOT IN (
-                        SELECT ns_sa_id
-                        FROM ns_shift_dropped)
-                ORDER BY d.ns_desk_shortname,s.ns_shift_date,s.ns_shift_start_time,c.ns_cat_uname
-                ";
+  $query = "
+    SELECT s.ns_shift_date, s.ns_shift_start_time, c.ns_cat_uname, d.ns_desk_shortname
+    FROM ns_shift as s, ns_shift_assigned as a, ns_desk as d, ns_cat as c
+    WHERE s.ns_shift_date >= ?
+    AND s.ns_shift_date <= ?
+    AND a.ns_cat_id = c.ns_cat_id
+    AND a.ns_desk_id = d.ns_desk_id
+    AND a.ns_shift_id = s.ns_shift_id
+    AND a.ns_sa_id NOT IN (
+      SELECT ns_sa_id
+      FROM ns_shift_dropped)
+    ORDER BY d.ns_desk_shortname,s.ns_shift_date,s.ns_shift_start_time,c.ns_cat_uname
+    ";
 
-        $sth = $dbh->prepare($query);
+  $sth = $dbh->prepare($query);
 
-        $sth->bindParam(1, $gsfa_start_date);
-        $sth->bindParam(2, $gsfa_end_date);
+  $sth->bindParam(1, $gsfa_start_date);
+  $sth->bindParam(2, $gsfa_end_date);
 
-        $sth->execute();
+  $sth->execute();
 
-        $sth->setFetchMode(PDO::FETCH_ASSOC);
+  $sth->setFetchMode(PDO::FETCH_ASSOC);
 
-        // Dump all the fetched data into a 2 dimensional array and return it.
-        $shifts = array();
-        while ($row = $sth->fetch()) {
-                $shifts[$row['ns_shift_date']][$row['ns_shift_start_time']][] = array(
-                        'ns_cat_uname' => $row['ns_cat_uname'],
-                        'ns_desk_shortname' => $row['ns_desk_shortname']);
-        };
+  // Dump all the fetched data into a 2 dimensional array and return it.
+  $shifts = array();
+  while ($row = $sth->fetch()) {
+    $shifts[$row['ns_shift_date']][$row['ns_shift_start_time']][] = array(
+      'ns_cat_uname' => $row['ns_cat_uname'],
+      'ns_desk_shortname' => $row['ns_desk_shortname']);
+  };
 
-        return $shifts;
+  return $shifts;
 };
 ?>
